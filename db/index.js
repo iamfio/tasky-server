04c5849aab00b8ce9ae6a19f73df0ced3be74545
Fiodor Gorobet <iamfiogo@gmail.com>
2022-12-15 23:40:01 +0100

    prepare cyclic deployment

--- db/index.js
+++ db/index.js
@@ -1,21 +1,43 @@
 // ℹ️ package responsible to make the connection with mongodb
 // https://www.npmjs.com/package/mongoose
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
 // Up Mongoose 7 standard settings
-mongoose.set('strictQuery', false)
+mongoose.set("strictQuery", false);
 
 // ℹ️ Sets the MongoDB URI for our app to have access to it.
 // If no env has been set, we dynamically set it to whatever the folder name was upon the creation of the app
 
-const MONGO_URI = process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/tasky-app'
+const MONGO_URI = process.env.MONGO_URI;
 
-mongoose
-  .connect(MONGO_URI)
-  .then((x) => {
-    const dbName = x.connections[0].name
-    console.log(`Connected to Mongo! Database name: "${dbName}"`)
-  })
-  .catch((err) => {
-    console.error('Error connecting to mongo: ', err)
-  })
+// mongoose
+//   .connect(MONGO_URI)
+//   .then((x) => {
+//     const dbName = x.connections[0].name;
+//     console.log(`Connected to Mongo! Database name: "${dbName}"`);
+//   })
+//   .catch((err) => {
+//     console.error("Error connecting to mongo: ", err);
+//   });
+
+// const mongoConnect = () => {
+//   return mongoose
+//     .connect(MONGO_URI)
+//     .then((x) => {
+//       const dbName = x.connections[0].name;
+//       console.log(`Connected to Mongo! Database name: "${dbName}"`);
+//     })
+//     .catch((err) => {
+//       console.error("Error connecting to mongo: ", err);
+//     });
+// };
+
+export const mongoConnect = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.MONGO_URI);
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
+  }
+}
